dbbc3bf7131584b85699553ba4bee240587d9397
AartiPundalik <aartipundalik68@gmail.com>
2023-02-23 16:27:51 +0530

    [Aarti] Add : UC1-Adding Simple Linked List

---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedlist = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Please enter an option");
+                Console.WriteLine("1:");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedlist.Add(56);
+                        linkedlist.Add(30);
+                        linkedlist.Add(70);
+                        break;
+                    case 2:
+                        linkedlist.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option");
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                        Console.ReadLine();
+                }
+            }
+        }
+    }
+}
+        
+    
+
